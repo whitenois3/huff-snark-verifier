d7eac1e7d75ef8dacfc8b92263c6942f89c01a91
clabby <ben@clab.by>
2022-08-29 15:57:17 -0400

    More :broom:, getting there!

--- test/Verifier.t.sol
+++ test/Verifier.t.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.16;
 
 import "forge-std/Test.sol";
-import {Pairing, Verifier} from "../src/contracts/Verifier.sol";
+import {Pairing, Verifier as SingleInputVerifier} from "./single-input/Verifier.sol";
 import "foundry-huff/HuffDeployer.sol";
 
 interface IHuffVerifier {
@@ -14,19 +14,40 @@ interface IHuffVerifier {
 }
 
 contract VerifierTest is Test {
-    Verifier public verifier;
-    IHuffVerifier public huffVerifier;
+    SingleInputVerifier public solSingleInputVerifier;
+    IHuffVerifier public huffSingleInputVerifier;
+
+    // TODO: Multiple input proof tests
 
     function setUp() public {
         string memory wrapper_code = vm.readFile("test/wrappers/VerifierWrapper.huff");
-        huffVerifier = IHuffVerifier(HuffDeployer.deploy_with_code("contracts/Verifier", wrapper_code));
-        verifier = new Verifier();
+
+        // Single Input Verifiers
+        huffSingleInputVerifier = IHuffVerifier(HuffDeployer.deploy_with_code("../test/single-input/SampleVerifier", wrapper_code));
+        solSingleInputVerifier = new SingleInputVerifier();
+    }
+
+    function testGasSingleInput() public {
+        (
+            uint[2] memory a,
+            uint[2][2] memory b,
+            uint[2] memory c,
+            uint[] memory inputs
+        ) = getSingleInputProof();
+
+        uint g = gasleft();
+        huffSingleInputVerifier.verify(a, b, c, inputs);
+        emit log_named_uint("gas", g - gasleft());
+
+        g = gasleft();
+        solSingleInputVerifier.verifyProof(a, b, c, inputs);
+        emit log_named_uint("gas", g - gasleft());
     }
 
     function testNegate() public {
         Pairing.G1Point memory point = Pairing.G1Point(1, 2);
         Pairing.G1Point memory solRes = Pairing.negate(point);
-        Pairing.G1Point memory huffRes = huffVerifier.negate(point);
+        Pairing.G1Point memory huffRes = huffSingleInputVerifier.negate(point);
 
         assertEq(abi.encode(solRes), hex"000000000000000000000000000000000000000000000000000000000000000130644e72e131a029b85045b68181585d97816a916871ca8d3c208c16d87cfd45");
         assertEq(abi.encode(solRes), abi.encode(huffRes));
@@ -36,7 +57,7 @@ contract VerifierTest is Test {
         Pairing.G1Point memory a = Pairing.G1Point(1, 2);
         Pairing.G1Point memory b = Pairing.G1Point(1, 2);
         Pairing.G1Point memory solRes = Pairing.addition(a, b);       
-        Pairing.G1Point memory huffRes = huffVerifier.addition(a, b);       
+        Pairing.G1Point memory huffRes = huffSingleInputVerifier.addition(a, b);       
 
         assertEq(abi.encode(solRes), hex"030644e72e131a029b85045b68181585d97816a916871ca8d3c208c16d87cfd315ed738c0e0a7c92e7845f96b2ae9c0a68a6a449e3538fc7ff3ebf7a5a18a2c4");
         assertEq(abi.encode(solRes), abi.encode(huffRes));
@@ -46,33 +67,31 @@ contract VerifierTest is Test {
         Pairing.G1Point memory a = Pairing.G1Point(1, 2);
         uint s = 2;
         Pairing.G1Point memory solRes = Pairing.scalar_mul(a, s);       
-        Pairing.G1Point memory huffRes = huffVerifier.scalar_mul(a, s);       
+        Pairing.G1Point memory huffRes = huffSingleInputVerifier.scalar_mul(a, s);       
 
         assertEq(abi.encode(solRes), hex"030644e72e131a029b85045b68181585d97816a916871ca8d3c208c16d87cfd315ed738c0e0a7c92e7845f96b2ae9c0a68a6a449e3538fc7ff3ebf7a5a18a2c4");
         assertEq(abi.encode(solRes), abi.encode(huffRes));
     }
 
-    function testPairing() public {
-        Pairing.G1Point[] memory g1_points = new Pairing.G1Point[](2);
-        g1_points[0] = Pairing.P1();
-        g1_points[1] = Pairing.negate(Pairing.P1());
+    function testVerifySingleInputProof() public {
+        (
+            uint[2] memory a,
+            uint[2][2] memory b,
+            uint[2] memory c,
+            uint[] memory inputs
+        ) = getSingleInputProof();
 
-        Pairing.G2Point[] memory g2_points = new Pairing.G2Point[](2);
-        g2_points[0] = Pairing.P2();
-        g2_points[1] = Pairing.P2();
-
-        bool solRes = Pairing.pairing(g1_points, g2_points);
-
-        assertEq(solRes, true);
+        assertEq(solSingleInputVerifier.verifyProof(a, b, c, inputs), true);
+        assertEq(huffSingleInputVerifier.verify(a, b, c, inputs), true);
     }
 
-    function testVerifyProof() public {
-        uint[2] memory a = [
+    function getSingleInputProof() internal pure returns(uint[2] memory a, uint[2][2] memory b, uint[2] memory c, uint[] memory inputs) {
+        a = [
             0x0fa55097071458e4e105175cafdd75c0b4c6f639227db56ab908050dc99c6220,
             0x0ac50ef419a1e1f29f5ff0d1898af514a36bc42ba970fd503060a6123c6d8f6c
         ];
 
-        uint[2][2] memory b = [
+        b = [
             [
                 0x23cfed79e17316667872d9377d23c9553321bf162b566facf05f262fe1a534ca,
                 0x0a658672d02e119820017f116b913ec347182ba17b24416b683bee1d07a200db
@@ -83,23 +102,13 @@ contract VerifierTest is Test {
             ]
         ];
 
-        uint[2] memory c = [
+        c = [
             0x2038751350aad5781c0e5c1f245d75d6ea198ca6bc42440ae1fc74ee99841921,
             0x06ab85db3cf7d039bb4da84c2d6137ff93c3490a79146548949d1531a3455353
         ];
 
-        uint[] memory inputs = new uint[](1);
-        inputs[0] = uint(182377969136052884622247920755931704678273168534);
-        
-        uint g = gasleft();
-        huffVerifier.verify(a, b, c, inputs);
-        emit log_named_uint("gas", g - gasleft());
-
-        g = gasleft();
-        verifier.verifyProof(a, b, c, inputs);
-        emit log_named_uint("gas", g - gasleft());
-
-        assertEq(verifier.verifyProof(a, b, c, inputs), true);
-        assertEq(huffVerifier.verify(a, b, c, inputs), true);
+        uint[] memory _inputs = new uint[](1);
+        _inputs[0] = uint(182377969136052884622247920755931704678273168534);
+        inputs = _inputs;
     }
 }
